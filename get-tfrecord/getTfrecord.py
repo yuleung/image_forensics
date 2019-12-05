2544bb90f5a3e5a489dbbd84f2db9d65a2fd0aa7
yuleung <leungyu5@outlook.com>
2019-12-05 18:36:53 +0800

    Update getTfrecord.py

--- get-tfrecord/getTfrecord.py
+++ get-tfrecord/getTfrecord.py
@@ -1,4 +1,3 @@
-#get-tfrecord file
 import tensorflow as tf
 import numpy as np
 import os
@@ -13,23 +12,35 @@ def _int64_features(value):
     return tf.train.Feature(int64_list = tf.train.Int64List(value=[value]))
     
 
-def Sobel(img):
+def Scharr(img, threshold):
     x = cv2.Scharr(img, cv2.CV_16S, 1, 0)
     y = cv2.Scharr(img, cv2.CV_16S, 0, 1)
     dst = cv2.addWeighted(abs(x), 0.5, abs(y), 0.5, 0)
-    dst = np.clip(dst,0,191)
+    dst = np.clip(dst,0,threshold)
     return dst
+    
+'''
+def Sobel(img, Ksize = 3):
+    x = cv2.Sobel(img, cv2.CV_16S, 1, 0,ksize = Ksize)
+    y = cv2.Sobel(img, cv2.CV_16S, 0, 1, ksize = Ksize)
+    dst = cv2.addWeighted(absX, 0.5, absY, 0.5, 0)
+    dst = np.clip(dst,0,threshold)
+    return dst
+'''
 
-def GetMultichannel(imgRGB):
+
+def getGLCM(imgRGB,threshold):
     imgYCrCb = cv2.cvtColor(imgRGB, cv2.COLOR_BGR2YCrCb)
-    imgYCrCb1 = Sobel(imgYCrCb[:,:,1])
-    imgYCrCb2 = Sobel(imgYCrCb[:,:,2])
-    imgYCrCb1 = feature.texture.greycomatrix(imgYCrCb1, [1], [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4], levels=192)[:,:,0,:]
-    imgYCrCb2 = feature.texture.greycomatrix(imgYCrCb2, [1], [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4], levels=192)[:, :, 0, :]
-    IMG = np.concatenate((imgYCrCb1,imgYCrCb2),axis=2)
+    imgCr = Scharr(imgYCrCb[:,:,1], threshold)
+    imgCb = Scharr(imgYCrCb[:,:,2], threshold)
+    #imgCr = Sobel(imgYCrCb[:,:,1], threshold)
+    #imgCb = Sobel(imgYCrCb[:,:,2], threshold)
+    GLCMofCr = feature.texture.greycomatrix(imgCr, [1], [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4], levels=threshold+1)[:, :, 0, :]
+    GLCMofCb = feature.texture.greycomatrix(imgCb, [1], [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4], levels=threshold+1)[:, :, 0, :]
+    IMG = np.concatenate((GLCMofCr,GLCMofCb),axis=2)
     return IMG
 
-def GetLable(FileName):    #AU:0 TP:1 GAN:2
+def getLable(FileName):    #AU:0 TP:1 GAN:2
     Split = FileName.split('_')
     if Split[0] == 'BGAN':
         return 2
@@ -38,60 +49,50 @@ def GetLable(FileName):    #AU:0 TP:1 GAN:2
     else:
         return 0
 
-'''
-def GetLable(FileName):    #AU:0 TP:1 GAN:2
-    if len(FileName)>25:
-        return 0
-    else:
-        return 1
-      '''
-#获得数据目录
-#PathPreTrain = r'/home/manager/SeXceptionResnet/Dataset/NEW/train_casia1-2_4844'
-#PathRecord = r'/home/manager/SeXceptionResnet/Dataset/NEW_tfrecord/train_casia1-2_4844'
-#PathPreTrain = r'/home/manager/SeXceptionResnet/Dataset/NEW/test_casia1-2_1200'
-#PathRecord = r'/home/manager/SeXceptionResnet/Dataset/NEW_tfrecord/test_casia1-2_1200'
 
-#PathPreTrain = r'/home/manager/SeXceptionResnet/Dataset/NEW/train_styleGan'
-#PathRecord = r'/home/manager/SeXceptionResnet/Dataset/NEW_tfrecord/train_scharr_256_stylegan_10000'
-#PathPreTrain = r'/home/manager/SeXceptionResnet/Dataset/NEW/test_styleGan'
-#PathRecord = r'/home/manager/SeXceptionResnet/Dataset/NEW_tfrecord/test_scharr_256_stylegan_3000'
-#PathPreTrain = r'/home/manager/SeXceptionResnet/Dataset/NEW/train_4123'
-#PathRecord = r'/home/manager/SeXceptionResnet/Dataset/NEW_tfrecord/train_scharr_160_4123'
-PathPreTrain = r'/home/manager/SeXceptionResnet/Dataset/NEW/test_1000'
-PathRecord = r'/home/manager/SeXceptionResnet/Dataset/NEW_tfrecord/test_scharr_160_1000'
-#PathPreTrain = r'/home/manager/SeXceptionResnet/Dataset/NEW/test_1000'
-#PathRecord = r'/home/manager/SeXceptionResnet/Dataset/NEW_tfrecord/test_scharr_288_1000'
-
-#PathPreTrain = r'/home/manager/SeXceptionResnet/Dataset/NEW/train_casia1-2_4844'
-#PathRecord = r'/home/manager/SeXceptionResnet/Dataset/NEW_tfrecord/train_casia1-2_4844'
-#PathPreTrain = r'/home/manager/SeXceptionResnet/Dataset/NEW/test_casia1-2_1200'
-#PathRecord = r'/home/manager/SeXceptionResnet/Dataset/NEW_tfrecord/test_casia1-2_1200'
-##获得目录下的文件名存为list
-FileOfPreTrain = os.listdir(PathPreTrain)
-
-#打乱List中数据
-random.shuffle(FileOfPreTrain)
-
-#获得文件并生成多通道图像和Lable --->Tfrecord  Test
-imgNumber = 0
-RecordFileNum = 0
-TfrecordsFilename = ('train_%.3d_.tfrecord' % RecordFileNum)
-Path = os.path.join(PathRecord, TfrecordsFilename)
-writer = tf.python_io.TFRecordWriter(Path)
-for FileName in FileOfPreTrain:
-    FilePath = os.path.join(PathPreTrain, FileName)
-    img = cv2.imread(FilePath)   #.shape(heigth,width)
-    img = GetMultichannel(img)
-    img = img.astype(np.float32)
-    lable = GetLable(FileName)
-    imgString = img.tobytes()
-    example = tf.train.Example(features = tf.train.Features(feature ={
-                       'imaString': _bytes_features(imgString),
-                        'label': _int64_features(lable)
-                        }))
-    if imgNumber % 100 == 0:
-        print(imgNumber)
-    writer.write(example.SerializeToString())
-    imgNumber += 1
-print('Done the %d Tfrecord of test'%RecordFileNum)
-writer.close()
+def getTfrecord(pathOfFile, pathOfSaveRecord, threshold):
+    Files = os.listdir(pathOfFile)
+    #shuffle the data
+    random.shuffle(Files)
+    #to count how many img have been processed
+    imgNumber = 0
+    #when the tfrecord file is too large, you can set a looping conditions for sharding
+    RecordFileNum = 0
+    TfrecordsFilename = ('train_%.3d_.tfrecord' % RecordFileNum)
+    Path = os.path.join(pathOfSaveRecord, TfrecordsFilename)
+    writer = tf.python_io.TFRecordWriter(Path)
+    for fileName in Files:
+        FilePath = os.path.join(pathOfFile, fileName)
+        img = cv2.imread(FilePath)  
+        img = getGLCM(img,threshold)
+        img = img.astype(np.float32)
+        lable = getLable(fileName)
+        imgString = img.tobytes()
+        example = tf.train.Example(features = tf.train.Features(feature ={
+                           'imaString': _bytes_features(imgString),
+                            'label': _int64_features(lable)
+                            }))
+        if imgNumber % 100 == 0:
+            print('%d pictures have been processed' %imgNumber)
+        writer.write(example.SerializeToString())
+        imgNumber += 1
+    writer.close()
+    
+    
+if __name__ == '__main__':
+    #the path of the train images, you should change it to your path
+    pathOfTrainFile = r'/home/manager/LY/Dataset/train'   
+    #the path to save the tfrecord file of train, you should change it to your path
+    pathOfTrainRecord = r'/home/manager/LY/Dataset/train_scharr_192'     
+    #the path of the test images, you should change it to your path
+    pathOfTestFile = r'/home/manager/LY/Dataset/test_1000'    
+    #the path to save the tfrecord file of train, you should change it to your path
+    pathOfTestRecord = r'/home/manager/LY/Dataset/tfrecord/test_scharr_192'     
+    #this threshold is described in the paper
+    threshold = 191  
+    
+    getTfrecord(pathOfTrainFile, pathOfTrainRecord, threshold)
+    print('Done of the tfrecord of test!')
+    getTfrecord(pathOfTestFile, pathOfTestRecord, threshold)
+    print('Done of the tfrecord of test!')
+    print('Done!')
